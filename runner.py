acb8fde8cfff7a98306e43e4161fbee09ef7c638
Vishal-Singh27 <vskota273@gmail.com>
2023-09-05 07:59:31 +0530

    Added label at top of root

--- runner.py
+++ runner.py
@@ -1,10 +1,14 @@
 # Importing necessary libraries
-from tkinter import Tk, Button, Toplevel
-from tkinterbuttonclass import Buttons, EMPTY, X, O
+from tkinter import Tk, Button, Toplevel, Label
+from tkinterbuttonclass import Buttons, O
 
 # Starting our root window
 root = Tk()
+root.title("Tic-Tac-Toe by Vishal Singh")
 
+# Label at top
+label = Label(root, text="Tic Tac Toe by Vishal Singh")
+label.grid(row=0, column=3)
 
 def start():    
     newwindow = Toplevel()
@@ -14,21 +18,25 @@ def start():
 
 # If user chooses to play as X
 def playasx(window):
+    global label
     window.destroy()
     startbutton.destroy()
-    Buttons(root, padx=70, pady=70, columnspan=2)
+    label.destroy()
+    Buttons(root, padx=50, pady=50, columnspan=2)
 
 
 # If user chooses to play as O
 def playaso(window):
+    global label
     window.destroy()
     startbutton.destroy()
-    Buttons(root, player=O, padx=70, pady=70, columnspan=2)
+    label.destroy()
+    Buttons(root, player=O, padx=50, pady=50, columnspan=2)
 
 
 # A button to start the game
 startbutton = Button(text="Start", command=start, padx=10, pady=10)
-startbutton.grid(row=0, column=3)
+startbutton.grid(row=1, column=3)
 
 # Starting the main event loop
 root.mainloop()
