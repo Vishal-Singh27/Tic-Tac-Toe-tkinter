7b4d15a89e5db4a801f4f3cd13264a858991c693
Vishal-Singh27 <vskota273@gmail.com>
2023-09-08 05:22:15 +0530

    Completed runner.py too, now only alpha pruning left in boardlogic.py

--- runner.py
+++ runner.py
@@ -1,6 +1,6 @@
 # Importing necessary libraries
 from tkinter import Tk, Button, Toplevel, Label
-from tkinterbuttonclass import Buttonsforai, O
+from tkinterbuttonclass import Buttonsforai, O, Buttonsforpvp
 
 # Starting our root window
 root = Tk()
@@ -8,9 +8,9 @@ root.title("Tic-Tac-Toe by Vishal Singh")
 
 # Label at top
 label = Label(root, text="Tic Tac Toe by Vishal Singh")
-label.grid(row=0, column=3)
+label.grid(row=0, column=0, columnspan=3)
 
-def start():    
+def withAI():    
     newwindow = Toplevel()
     Button(newwindow, text="Play as X", command=lambda: playasx(newwindow)).pack()
     Button(newwindow, text="Play as O", command=lambda: playaso(newwindow)).pack()
@@ -21,6 +21,7 @@ def playasx(window):
     global label
     window.destroy()
     aibutton.destroy()
+    humanbutton.destroy()
     label.destroy()
     Buttonsforai(root, padx=50, pady=50, columnspan=2)
 
@@ -32,15 +33,23 @@ def playaso(window):
     aibutton.destroy()
     label.destroy()
     Buttonsforai(root, player=O, padx=50, pady=50, columnspan=2)
+    
+
+def PVP():
+    global label
+    aibutton.destroy()
+    humanbutton.destroy()
+    label.destroy()
+    Buttonsforpvp(root, padx=50, pady=50, columnspan=2)
 
 
 # Button to start the game vs AI
-aibutton = Button(text="PlaywithAI", command=start, padx=10, pady=10)
-aibutton.grid(row=1, column=3)
+aibutton = Button(text="Play with AI", command=withAI, padx=10, pady=10)
+aibutton.grid(row=1, column=1)
 
 # Button to start the game vs human
-humanbutton = Button(text="PlaywithAI", command=start, padx=10, pady=10)
-humanbutton.grid(row=1, column=3)
+humanbutton = Button(text="Play PVP", command=PVP, padx=15, pady=10)
+humanbutton.grid(row=1, column=2)
 
 # Starting the main event loop
 root.mainloop()
